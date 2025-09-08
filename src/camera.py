2733bfa7c3727e8da0a2625192da9c3d69332461
ydna985 <andy-abdoul-mouhaimine.koutou@polymtl.ca>
2025-09-08 17:40:56 -0400

    added computer_vision file

--- src/camera.py
+++ src/camera.py
@@ -6,6 +6,7 @@ import pyzed.sl as sl
 class Camera:
     __lock = Lock() # i don<t know if it is better inside or outside the class
     __zed = None
+    __init_params = None
         
     @staticmethod
     def get_camera(svo=None):
@@ -20,17 +21,27 @@ class Camera:
                     input_type.set_from_svo_file(svo) 
 
                 # Set the configuration parameters
-                init_params = sl.InitParameters(input_t=input_type, svo_real_time_mode=True)
-                init_params.coordinate_units = sl.UNIT.METER
-                init_params.depth_mode = sl.DEPTH_MODE.ULTRA
-                init_params.coordinate_system = sl.COORDINATE_SYSTEM.RIGHT_HANDED_Y_UP
-                init_params.depth_maximum_distance = 3
-                init_params.camera_fps = 60
+                Camera.__init_params = sl.InitParameters(input_t=input_type, svo_real_time_mode=True)
+                Camera.__init_params.coordinate_units = sl.UNIT.METER
+                Camera.__init_params.depth_mode = sl.DEPTH_MODE.ULTRA
+                Camera.__init_params.coordinate_system = sl.COORDINATE_SYSTEM.RIGHT_HANDED_Y_UP
+                Camera.__init_params.depth_maximum_distance = 3
+                Camera.__init_params.camera_fps = 60
 
                 #should i make a open and close method
 
 
         return Camera.__zed
+    
+    def open():
+        status = Camera.__zed.open(Camera.__init_params)
+
+        if status != sl.ERROR_CODE.SUCCESS:
+            print(repr(status))
+            exit()
+
+    def close():
+        Camera.__zed.close()
 
     
 
