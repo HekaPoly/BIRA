3dd49747bdb84fcc9d954517c3ee6eb9d945479e
ydna985 <andy-abdoul-mouhaimine.koutou@polymtl.ca>
2025-09-08 13:50:07 -0400

    changed  static method

--- src/cv_viewer/camera.py
+++ src/cv_viewer/camera.py
@@ -1,43 +1,49 @@
-from threading import Lock, Thread
+from threading import Lock
 import pyzed.sl as sl
 
 
 
-lock=Lock()
-
 class Camera:
-
-    def __init__(self, opt):
-        self.__private_field="Private"
-        self.opt = opt
+    __lock = Lock() # i don<t know if it is better inside or outside the class
+    __zed = None
+    __input_type = None
+    __init_params = None
+    __runtime_params = None
         
     @staticmethod
-    def get_private_field(self):
+    def get_camera(svo=None):
 
-        if Camera.__private_field == None:
-            lock.acquire()
+        if Camera.__zed == None:
+            with Camera.__lock:
 
-            if Camera.__private_field == None:
-                Camera.__private_field = sl.Camera
-                input_type = sl.InputType()
+                if Camera.__zed == None:
+                    Camera.__zed = sl.Camera()
 
-                if self.opt.svo is not None:
-                    input_type.set_from_svo_file(self.opt.svo) 
+                    #if the order doesn<t matter i will move this to the constructeur
+                    Camera.__input_type = sl.InputType()
 
-                init_params = sl.InitParameters(input_t=input_type, svo_real_time_mode=True)
-                init_params.coordinate_units = sl.UNIT.METER
-                init_params.depth_mode = sl.DEPTH_MODE.ULTRA
-                init_params.coordinate_system = sl.COORDINATE_SYSTEM.RIGHT_HANDED_Y_UP
-                init_params.depth_maximum_distance = 3
-                init_params.camera_fps = 60
+                    if svo is not None:
+                        Camera.__input_type.set_from_svo_file(svo) 
 
-                status = Camera.__private_field.open(init_params)
-                if status != sl.ERROR_CODE.SUCCESS:
-                    print(repr(status))
-                    exit()
+                    # Set the configuration parameters
+                    Camera.__init_params = sl.InitParameters(input_t=Camera.__input_type, svo_real_time_mode=True)
+                    Camera.__init_params.coordinate_units = sl.UNIT.METER
+                    Camera.__init_params.depth_mode = sl.DEPTH_MODE.ULTRA
+                    Camera.__init_params.coordinate_system = sl.COORDINATE_SYSTEM.RIGHT_HANDED_Y_UP
+                    Camera.__init_params.depth_maximum_distance = 3
+                    Camera.__init_params.camera_fps = 60
+
+                    Camera.__runtime_params = sl.RuntimeParameters()
+
+                    #should i make a open method
+                    status = Camera.__zed.open(Camera.__init_params)
+                    if status != sl.ERROR_CODE.SUCCESS:
+                        print(repr(status))
+                        exit()
 
 
-        return Camera.__private_field
+        return Camera.__zed
+
     
 
     
