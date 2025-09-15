1ecfb53e451516d830565b312924e72e01809838
ydna985 <andy-abdoul-mouhaimine.koutou@polymtl.ca>
2025-09-15 12:09:14 -0400

    computer_vision: added camera to the class

--- src/computer_vision.py
+++ src/computer_vision.py
@@ -7,7 +7,7 @@ import time
 import cv_viewer.tracking_viewer as cv_viewer
 import history as rd
 import cv_viewer.labels as lab
-import camera
+from camera import Camera
 
 
 
@@ -122,26 +122,8 @@ class ComputerVision:
         capture_thread.start()
         print("Initializing Camera...")
 
-        zed = sl.Camera()
-
-        input_type = sl.InputType()
-        if opt.svo is not None:
-            input_type.set_from_svo_file(opt.svo)
-
-        # Create an InitParameters object and set the configuration parameters
-        init_params = sl.InitParameters(input_t=input_type, svo_real_time_mode=True)
-        init_params.coordinate_units = sl.UNIT.METER
-        init_params.depth_mode = sl.DEPTH_MODE.ULTRA
-        init_params.coordinate_system = sl.COORDINATE_SYSTEM.RIGHT_HANDED_Y_UP
-        init_params.depth_maximum_distance = 3
-        init_params.camera_fps = 60
-
-        runtime_params = sl.RuntimeParameters()
-        status = zed.open(init_params)
-
-        if status != sl.ERROR_CODE.SUCCESS:
-            print(repr(status))
-            exit()
+        zed = Camera(opt.svo)
+        zed.open()
 
         image_left_tmp = sl.Mat()
 
