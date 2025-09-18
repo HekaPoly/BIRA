0fa04996c3c473565bdcbaba4947ba80a5e02c55
VictoCri <victoria98128@outlook.com>
2025-09-18 11:26:55 -0400

    Fixing minor mistakes (in the constructor) that affected the whole code

--- src/uart_transmitter.py
+++ src/uart_transmitter.py
@@ -8,14 +8,14 @@ from serial.tools import list_ports
 from time import sleep
 
 class UARTTransmitter:
-    def __init__(self, DEFAULT_BAUD_RATE, VELOCITY_SHIFT, ANGLE_SHIFT, NUMBER_OF_BYTES, UART_INIT_DELAY):
-        self.DEFAULT_BAUD_RATE = 115200
-        self.VELOCITY_SHIFT = 3
-        self.ANGLE_SHIFT = 16 
-        self.NUMBER_OF_BYTES = 4
-        self.UART_INIT_DELAY = 2
+    def __init__(self, DEFAULT_BAUD_RATE = 115200, VELOCITY_SHIFT = 3, ANGLE_SHIFT = 16, NUMBER_OF_BYTES = 4, UART_INIT_DELAY = 2):
+        self.DEFAULT_BAUD_RATE = DEFAULT_BAUD_RATE
+        self.VELOCITY_SHIFT = VELOCITY_SHIFT
+        self.ANGLE_SHIFT = ANGLE_SHIFT
+        self.NUMBER_OF_BYTES = NUMBER_OF_BYTES
+        self.UART_INIT_DELAY = UART_INIT_DELAY
 
-    def get_serial_ports_list() -> list:
+    def get_serial_ports_list(self) -> list:
         """ 
         This function returns a list of active serial ports;
 
@@ -37,7 +37,7 @@ class UARTTransmitter:
 
         return list_com_ports
 
-    def send_data_through_UART(angle: int, motor_id: int = 0) -> bool:
+    def send_data_through_uart(self, angle: int, motor_id: int = 0) -> bool:
         """
         This function takes angle as input to send it to a microcontroller through UART;
 
@@ -49,25 +49,25 @@ class UARTTransmitter:
         """
         angle =  int((2.15*int(angle)+360) % 360)
         assert(angle >= 0 and angle <= 360)
-        serial_ports = UARTTransmitter.get_serial_ports_list()
+        serial_ports = self.get_serial_ports_list()
         if len(serial_ports) != 1:
             raise Exception("Erreur: il ne doit y avoir qu'un seul port serie connecte")
         
         serial_port = serial_ports[0]
 
-        VELOCITY = 75
-        velocity <<= UARTTransmitter.VELOCITY_SHIFT
+        velocity = 75
+        velocity <<= self.VELOCITY_SHIFT
 
-        angle <<= UARTTransmitter.ANGLE_SHIFT
+        angle <<= self.ANGLE_SHIFT
         data_successfully_sent = False
         data = 0x00000000
         data += angle
         data += motor_id
-        data += VELOCITY
+        data += velocity
 
         ser = Serial(
                         port            = serial_port,
-                        baudrate        = UARTTransmitter.DEFAULT_BAUD_RATE,
+                        baudrate        = self.DEFAULT_BAUD_RATE,
                         timeout         = None,
                         write_timeout   = 0,
                         xonxoff         = False,
@@ -79,10 +79,10 @@ class UARTTransmitter:
             print(e)
             print("Unable to open serial communication port. Try selecting a different port.")
 
-        byte_data = data.to_bytes(UARTTransmitter.NUMBER_OF_BYTES, byteorder='little')
+        byte_data = data.to_bytes(self.NUMBER_OF_BYTES, byteorder='little')
 
         try:
-            sleep(UARTTransmitter.UART_INIT_DELAY)
+            sleep(self.UART_INIT_DELAY)
             
             ser.write(byte_data)
             data_successfully_sent = True
