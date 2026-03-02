4a113c9c7ed752b3ca91db52f69a4fc2ac756e8e
rayanajakane <rayan.ajakane@gmail.com>
2026-03-02 15:16:30 -0500

    Added context and change_state structure
    Co-authored-by: Phi-Dan <Phi-Dan@users.noreply.github.com>

--- src/bira_components/manager.py
+++ src/bira_components/manager.py
@@ -4,13 +4,14 @@ from bira_components.computer_vision import ComputerVision
 from bira_components.mediator import BiraMediator
 from bira_components.micro import Micro
 from bira_components.speech_to_text import SpeechToText
+from bira_components.states import IdleState
 from bira_components.text_to_speech import TextToSpeech
 from bira_components.uart_transmitter import UARTTransmitter
 
 
 class BIRA_Manager:
     def __init__(self):
-        self.mediator = BiraMediator()
+        # self.mediator = BiraMediator()
         self.camera = Camera(self.mediator)
         self.computer_vision = ComputerVision(self.mediator)
         self.uart_transmitter = UARTTransmitter(self.mediator)
@@ -18,3 +19,19 @@ class BIRA_Manager:
         self.text_to_speech = TextToSpeech(self.mediator)
         self.speech_to_text = SpeechToText(self.mediator)
         self.slm_manager = SLM_Manager(self.mediator)
+        self.state = IdleState(self)
+        self.context = {
+            "objects_detected": [],
+            "user_input": None,
+            "feedback": None,
+            "object_selected": None
+        }
+        self.counter = 0
+    
+    def change_state(self, new_state):
+        self.state = new_state
+        print(f"State changed to: {self.state.get_name()}")
+
+    def handle(self, message):
+        self.state.handle(message)
+        pass
