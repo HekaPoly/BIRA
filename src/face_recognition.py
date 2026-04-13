216fd1d319c66160086413e9ed92193fc375aaa9
ydna985 <andy-abdoul-mouhaimine.koutou@polymtl.ca>
2026-04-12 21:14:20 -0400

    clean up

--- src/face_recognition.py
+++ src/face_recognition.py
@@ -2,14 +2,11 @@
 # for that we can use opencv's built in har cascade or the pretrained model in dlib
 # after that we need to use a facial landmarks detector for that we can also use dlib
 # get more info here https://pyimagesearch.com/2017/04/03/facial-landmarks-dlib-opencv-python/
-# api for dlib https://dlib.net/python/
 # how to download dlib https://pyimagesearch.com/2017/03/27/how-to-install-dlib/ and https://github.com/davisking/dlib
-# example https://dlib.net/face_landmark_detection.py.html
-# the facial landmark model used can be find on https://dlib.net/files/ and is named
-# shape_predictor_68_face_landmarks.dat.bz2 other model exits
-# I aslo found another way to do it using MediaPipe Face Mesh
+# the facial landmark model used can be find on https://dlib.net/files/
+# I also found another way to do it using MediaPipe Face Mesh
 # the missing methods can be found here https://github.com/PyImageSearch/imutils
-# emotion model link https://github.com/niebardzo/Emotions
+# emotion model repo https://github.com/niebardzo/Emotions
 
 import numpy as np
 import dlib
@@ -18,12 +15,6 @@ import pyzed.sl as sl
 from camera import Camera
 from joblib import load
 from utils.image_processing import Face
-
-def convert_dlib_landmarks_to_np_array(landmarks) :
-    points = []
-    for i in range(landmarks.num_parts):
-        points.append((landmarks.part(i).x, landmarks.part(i).y))
-    return np.array(points, dtype=np.int32)
 
 def convert_dlib_BB_to_openCV_BB(rect):
     x = rect.left()
@@ -40,10 +31,9 @@ if (__name__ == "__main__"):
     #load the emotion model
     emotion_model = load('../models/emotion.joblib')
 
-    # we need to aquire the image with the zed sdk
     myCamera = Camera()
     image = None
-
+    # grab a frame
     with myCamera:
         if (myCamera.grab() == sl.ERROR_CODE.SUCCESS):
             image = myCamera.get_frame()
@@ -52,23 +42,16 @@ if (__name__ == "__main__"):
 
     # resizing the image can positvely impact the computing time
     #convert the image to grayscale
-    # image = cv2.imread("/home/nvidia/Desktop/Bira/src/Screenshot 2026-02-12 093020.png")
     grayScale_image = cv2.cvtColor(image, cv2.COLOR_BGRA2RGB)
     # upscale the image and get BB of the face can also work with RGB image
     face_BB = detector(grayScale_image, 1)
-    if image is None:
-        print("Image is None!")
-    else:
-        print("Image shape:", image.shape, "dtype:", image.dtype)
-    print("number of faces detected: {}".format(len(face_BB)))
+
     for BB in face_BB:
         face = Face(grayScale_image, BB, predictor)
         prediction = emotion_model.predict([face.extract_features()])
         #get the facial landmarks coordinates (x,y)
         #convert to openCv BB
         (x, y, w, h) = convert_dlib_BB_to_openCV_BB(BB)
-        print(x, y, w, h)
-        print("the emotion is {}".format(emotion_model.le.inverse_transform(prediction)[0]))
         #draw the BB
         cv2.rectangle(image, (x,y), (x+w, y+h), (0,255,0),2)
         cv2.putText(image, "###{}".format(emotion_model.le.inverse_transform(prediction)[0]), (x - 10, y - 10),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
@@ -79,4 +62,3 @@ if (__name__ == "__main__"):
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     cv2.destroyAllWindows()
-    
