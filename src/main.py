b4cd6308c1cb7349cbb0ea42287f8d1aac852273
rayanajakane <rayan.ajakane@gmail.com>
2026-04-15 13:34:30 -0400

    removed unsued parsing arguments

--- src/main.py
+++ src/main.py
@@ -8,10 +8,10 @@ def main():
     models_dir = Path(__file__).resolve().parents[1] / "models"
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', type=str, default=str(models_dir / 'yolov8n_resna.pt'), help='model.pt path(s)')
-    parser.add_argument('--svo', type=str, default=None, help='optional svo file')
-    parser.add_argument('--img_size', type=int, default=416, help='inference size (pixels)')
-    parser.add_argument('--conf_thres', type=float, default=0.4, help='object confidence threshold')
+    # parser.add_argument('--weights', type=str, default=str(models_dir / 'yolov8n_resna.pt'), help='model.pt path(s)')
+    # parser.add_argument('--svo', type=str, default=None, help='optional svo file')
+    # parser.add_argument('--img_size', type=int, default=416, help='inference size (pixels)')
+    # parser.add_argument('--conf_thres', type=float, default=0.4, help='object confidence threshold')
     parser.add_argument(
         '--mock',
         action='store_true',
